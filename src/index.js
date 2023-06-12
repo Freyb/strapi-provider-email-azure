d4632c9e1921207ab762250ec664fc94a64c48a9
Freyb <balazs.frey@gmail.com>
2023-06-12 19:30:43 +0200

    Fix bug with replyTo prop, Make senderAddress compatible with user-permissions plugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const { EmailClient } = require('@azure/communication-email');
 
-const additionalKeys = [
-  'attachments',
-  'replyTo',
-  'disableUserEngagementTracking',
-  'headers',
-];
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ValidationError';
+    this.message = message;
+  }
+}
+
+const additionalKeys = ['attachments', 'disableUserEngagementTracking', 'headers'];
+
+const emailRegex =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+const recipentWithEmailRegex = new RegExp(
+  `^(?<displayName>.*) <(?<address>${emailRegex.source.slice(1, -1)})>$`,
+);
+const isEmail = (text) => emailRegex.test(text);
+const isRecipentWithEmail = (text) => recipentWithEmailRegex.test(text);
 
 module.exports = {
   provider: 'azure',
@@ -14,9 +25,14 @@ module.exports = {
   init: (providerOptions = {}, settings = {}) => {
     const emailClient = new EmailClient(providerOptions.endpoint);
 
-    const transformAddress = (address) => {
-      if (!address) return address;
-      if (typeof address === 'string') return [{ address }];
+    const transformAddress = (address, defaultAddress) => {
+      if (!address) return defaultAddress ? [{ address: defaultAddress }] : undefined;
+      if (typeof address === 'string') {
+        if (isEmail(address)) return [{ address }];
+        if (isRecipentWithEmail(address))
+          return [address.match(recipentWithEmailRegex).groups];
+        throw new ValidationError(`Invalid email address: ${address}`);
+      }
       if (!Array.isArray(address) && address.hasOwnProperty('address')) return [address];
       return address;
     };
@@ -24,7 +40,8 @@ module.exports = {
     return {
       send: async (options) => {
         const message = {
-          senderAddress: options.from || settings.defaultFrom,
+          senderAddress: transformAddress(options.from, settings.defaultFrom)[0].address,
+          replyTo: transformAddress(options.replyTo, settings.defaultFrom),
           content: {
             subject: options.subject || '',
             plainText: options.text || '',
@@ -36,9 +53,7 @@ module.exports = {
             bcc: transformAddress(options.bcc),
           },
           ...Object.fromEntries(
-            Object.entries(options).filter(([key, _val]) =>
-              additionalKeys.includes(key),
-            ),
+            Object.entries(options).filter(([key, _val]) => additionalKeys.includes(key)),
           ),
         };
         const poller = await emailClient.beginSend(message);
